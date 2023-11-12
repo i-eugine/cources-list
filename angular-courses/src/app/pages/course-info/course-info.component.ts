0c63bc4e4017c7a20ae46986a0f52f72080abe18
Eugene Izotov <izotov1995@gmail.com>
2023-11-12 15:49:42 +0200

    Courses info page

---
+++ angular-courses/src/app/pages/course-info/course-info.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'co-course-info',
+	templateUrl: './course-info.component.html',
+	styleUrls: ['./course-info.component.scss'],
+})
+export class CourseInfoComponent {}
