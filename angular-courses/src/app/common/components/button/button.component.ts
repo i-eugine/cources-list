35702142ed4d80460fb0c125240daa736a358036
Eugene Izotov <izotov1995@gmail.com>
2023-11-12 15:47:11 +0200

    Button component

---
+++ angular-courses/src/app/common/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+	selector: 'co-button',
+	standalone: true,
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss'],
+	encapsulation: ViewEncapsulation.None,
+})
+export class ButtonComponent {}
