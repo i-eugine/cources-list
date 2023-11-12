d7e8276d147bb504eff3893a5c764ab0f64949b5
Eugene Izotov <izotov1995@gmail.com>
2023-11-12 15:50:47 +0200

    Add barrel import

---
+++ angular-courses/src/app/common/index.ts
@@ -0,0 +1,6 @@
+export * from './components/button/button.component';
+export * from './components/header/header.component';
+export * from './components/input/input.component';
+
+export * from './pipes/duration.pipe';
+export * from './pipes/format-date.pipe';
---
+++ angular-courses/src/app/models/index.ts
@@ -0,0 +1,2 @@
+export * from './i-author';
+export * from './i-course';
