0cc83c16832f6db4d921bb3f7c0dafb0cd66ef48
Eugene Izotov <izotov1995@gmail.com>
2023-12-27 00:57:33 +0200

    Add message module

---
+++ react-reforged/src/common/modules/message/message-keys.enum.ts
@@ -0,0 +1,10 @@
+export enum MESSAGE_KEYS {
+  AUTHOR_CREATE = 'author-create',
+  AUTHOR_DELETE = 'author-delete',
+
+  COURSE_DELETE = 'course-delete',
+  COURSE_SAVE = 'course-save',
+
+  USER_LOGIN = 'user-login',
+  USER_REGISTER = 'user-register',
+}
