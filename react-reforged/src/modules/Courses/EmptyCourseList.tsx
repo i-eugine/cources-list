1c0d7b1957c851082428042847ed3f1f4d4e0f61
Eugene Izotov <izotov1995@gmail.com>
2023-12-28 06:09:46 +0200

    Save

---
+++ react-reforged/src/modules/Courses/EmptyCourseList.tsx
@@ -0,0 +1,21 @@
+import { Button, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '@routing/routes';
+import { getHref } from '@utils/get-href';
+
+const { Title, Text } = Typography;
+
+export const EmptyCourseList = () => {
+  return (
+    <div>
+      <Title level={3}>Your List Is Empty</Title>
+      <Text className='mb-5 mt-10'>
+        Please use ’Add New Course’ button to add your first course
+      </Text>
+      <Link to={getHref(ROUTES.courses, ROUTES.create)}>
+        <Button>ADD NEW COURSE</Button>
+      </Link>
+    </div>
+  );
+};
