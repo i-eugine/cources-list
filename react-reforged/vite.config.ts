d5b524086fb038c2337585ac48d1cfd16b19dde4
Eugene Izotov <izotov1995@gmail.com>
2023-12-30 22:15:35 +0200

    Add pwa plugin

--- react-reforged/vite.config.ts
+++ react-reforged/vite.config.ts
@@ -1,6 +1,30 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> = {
+  name: 'React-vite-app',
+  short_name: 'react-vite-app',
+  description: 'I am a simple vite app',
+  icons: [
+    { src: '/maskable_icon.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+    { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'favicon' },
+    { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'favicon' },
+    {
+      src: '/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
+    },
+  ],
+  theme_color: '#171717',
+  background_color: '#f0e7db',
+  display: 'standalone',
+  scope: '/',
+  start_url: '/',
+  orientation: 'portrait',
+};
 
 const baseUrl = './src/';
 
@@ -21,10 +45,11 @@ const alias = [
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['@babel/plugin-proposal-decorators', { version: '2023-05' }]],
-      },
+    react({ babel: { plugins: [['@babel/plugin-proposal-decorators', { version: '2023-05' }]] } }),
+    VitePWA({
+      workbox: { globPatterns: ['**/*'] }, // cache all the imports
+      includeAssets: ['**/*'], // cache all thestatic assets in the public folder
+      manifest,
     }),
   ],
   resolve: { alias },
